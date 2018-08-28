560da44110c90ecd1537c754eb8f0187d784f367
deadlybulb <deadlybulb@orbital.enterprises>
2018-08-28 08:40:12 -0400

    update to latest SDE

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   platformServiceApiBasePath: 'https://evekit.orbital.enterprises/api',
-  sdeServiceApiBasePath: 'https://evekit-sde.orbital.enterprises/20180713/api/ws/v20180713',
+  sdeServiceApiBasePath: 'https://evekit-sde.orbital.enterprises/20180827/api/ws/v20180827',
   modelViewURL: 'https://evekit-model.orbital.enterprises/api/swagger.json',
   marketViewURL: 'https://evekit-market.orbital.enterprises/swagger',
   refViewURL: 'https://evekit-ref-model.orbital.enterprises/api/swagger.json'
