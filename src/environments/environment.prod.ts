92cef222950ee2b59fe2455555afcab5dc10f6ff
deadlybulb <deadlybulb@orbital.enterprises>
2019-05-17 07:28:31 -0400

    prep for release 20190516

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   platformServiceApiBasePath: 'https://evekit.orbital.enterprises/api',
-  sdeServiceApiBasePath: 'https://evekit-sde.orbital.enterprises/20190219/api/ws/v20190219',
+  sdeServiceApiBasePath: 'https://evekit-sde.orbital.enterprises/20190516/api/ws/v20190516',
   modelViewURL: 'https://evekit-model.orbital.enterprises/api/swagger.json',
   marketViewURL: 'https://evekit-market.orbital.enterprises/swagger',
   refViewURL: 'https://evekit-ref-model.orbital.enterprises/api/swagger.json',
